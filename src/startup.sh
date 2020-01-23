286279a7d627cc6ec602c86186701429a9f823df
Pinae <jonny@pinae.net>
2020-01-23 13:42:23 +0100

    Expose port 8081

--- src/startup.sh
+++ src/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.8/site-packages
 python manage.py migrate
-gunicorn --workers=3 AssetStorm.wsgi -b 0.0.0.0:8080
+gunicorn --workers=3 AssetStorm.wsgi -b 0.0.0.0:8081
