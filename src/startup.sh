626e5690bf444b9839b381a57ab4530436a7e876
Pinae <jonny@pinae.net>
2020-01-24 15:03:19 +0100

    Load data from fixtures on startup

--- src/startup.sh
+++ src/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.8/site-packages
 python manage.py migrate
+python manage.py loaddata *
 gunicorn --workers=3 AssetStorm.wsgi -b 0.0.0.0:8080
